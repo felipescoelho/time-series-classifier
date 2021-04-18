5057ea48574760fbecbac3f24beb0182b4e6bce9
felipescoelho <luizfscoelho.92@gmail.com>
2021-04-18 19:25:09 -0300

    Some corrections in the main script

--- graph_classification.py
+++ graph_classification.py
@@ -10,6 +10,8 @@ import dgl
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+from torch.utils.data.sampler import SubsetRandomSampler
+from dgl.dataloading import GraphDataLoader
 from dgl.data import DGLDataset
 from dgl.nn import GraphConv
 
@@ -54,8 +56,8 @@ class SyntheticDataset(DGLDataset):
 
             # Create a graph and add it to the list of graphs and
             # labels.
-            g = dgl.graph((src, dst), num_nodes=num_nodes)
-            self.graphs.append(g)
+            graph = dgl.graph((src, dst), num_nodes=num_nodes)
+            self.graphs.append(graph)
             self.labels.append(label)
 
         # Convert the label list to tenso for saving.
@@ -68,8 +70,57 @@ class SyntheticDataset(DGLDataset):
         return len(self.graphs)
 
 
+class GCN(nn.Module):
+    """Graph Convolutional Network model."""
+
+    def __init__(self, in_feats, h_feats, num_classes):
+        super(GCN, self).__init__()
+        self.conv1 = GraphConv(in_feats, h_feats)
+        self.conv2 = GraphConv(h_feats, num_classes)
+
+    def forward(self, graph, in_feat):
+        """Compute graph convolution."""
+        h_l = self.conv1(graph, in_feat)
+        h_l = F.relu(h_l)
+        h_l = self.conv2(graph, h_l)
+        graph.ndata['h'] = h_l
+        return dgl.mean_nodes(graph, 'h')
+
+
 if __name__ == '__main__':
     # Load the synthetic dataset:
     DATASET = SyntheticDataset()
     print('Node feature dimmensionality: ', DATASET.dim_nfeats)
     print('Number of graph categories: ', DATASET.gclasses)
+
+    # Define data loader:
+    NUM_EXAMPLES = len(DATASET)
+    NUM_TRAIN = int(NUM_EXAMPLES * .8)
+
+    TRAIN_SAMPLER = SubsetRandomSampler(torch.arange(NUM_TRAIN))
+    TEST_SAMPLER = SubsetRandomSampler(torch.arange(NUM_TRAIN, NUM_EXAMPLES))
+
+    TRAIN_DATALOADER = GraphDataLoader(DATASET, sampler=TRAIN_SAMPLER,
+                                       batch_size=5, drop_last=False)
+    TEST_DATALOADER = GraphDataLoader(DATASET, sampler=TEST_SAMPLER,
+                                      batch_size=5, drop_last=False)
+
+    MODEL = GCN(DATASET.dim_nfeats, 16, DATASET.gclasses)
+    OPTIMIZER = torch.optim.Adam(MODEL.parameters(), lr=0.01)
+
+    for epoch in range(20):
+        for batched_graph, labels in TRAIN_DATALOADER:
+            pred = MODEL(batched_graph, batched_graph.ndata['attr'].float())
+            loss = F.cross_entropy(pred, labels)
+            OPTIMIZER.zero_grad()
+            loss.backward()
+            OPTIMIZER.step()
+
+    NUM_CORRECT = 0
+    NUM_TESTS = 0
+    for batched_graph, labels in TEST_DATALOADER:
+        pred = MODEL(batched_graph, batched_graph.ndata['attr'].float())
+        NUM_CORRECT += (pred.argmax(1) == labels).sum().item()
+        NUM_TESTS += len(labels)
+
+    print('Test accuracy: {:.4f}'.format(NUM_CORRECT/NUM_TESTS))
